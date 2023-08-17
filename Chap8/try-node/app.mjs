f8b80ff455ff87966b5538f899faf3d9cf8b931f
gptus <yhc9308@naver.com>
2023-08-17 19:30:46 +0900

    간단한 노드 서버 만들기

--- Chap8/try-node/app.mjs
+++ Chap8/try-node/app.mjs
@@ -1,7 +1,10 @@
-import fs from "fs";
+import fs from "fs"; // 파일 시스템 모듈을 가져옵니다.
 
+// "test.txt" 파일에 "Hello World" 문자열을 작성합니다.
 fs.writeFile("test.txt", "Hello World", () => {
+  // 파일 작성이 완료되면 "test.txt" 파일을 읽어서 내용을 콘솔에 출력합니다.
   fs.readFile("test.txt", "utf8", (err, msg) => {
-    console.log(msg);
+    // err에는 오류 객체가 있을 수 있고, msg에는 읽은 파일의 내용이 포함됩니다.
+    console.log(msg); // 파일에서 읽은 문자열을 콘솔에 출력합니다.
   });
 });
